9f25ff99bbfcd894054bec8fe29429ddb5bfc11c
APIVerve Team <hello@apiverve.com>
2024-05-14 15:28:56 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,60 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class lemmas
+{
+    [JsonProperty("quick")]
+    public int quick { get; set; }
+
+    [JsonProperty("brown")]
+    public int brown { get; set; }
+
+    [JsonProperty("fox")]
+    public int fox { get; set; }
+
+    [JsonProperty("jump")]
+    public int jump { get; set; }
+
+    [JsonProperty("over")]
+    public int over { get; set; }
+
+    [JsonProperty("lazi")]
+    public int lazi { get; set; }
+
+    [JsonProperty("dog")]
+    public int dog { get; set; }
+
+    [JsonProperty("and")]
+    public int and { get; set; }
+
+    [JsonProperty("other")]
+    public int other { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("found")]
+    public int found { get; set; }
+
+    [JsonProperty("lemmas")]
+    public lemmas lemmas { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
